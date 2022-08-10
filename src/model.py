c1a587137782a1397aa3851ef25b68f9670297fe
moon <larcane@naver.com>
2022-08-10 08:31:43 +0000

    [update] change tag_has_music_list format
    - list to dict(tag_id:music_id_list[])
    - user tag include tag frequency

--- src/model.py
+++ src/model.py
@@ -22,7 +22,7 @@ class FrequencyModel(RecommendModel):
     def __init__(self):
         self.tag_has_music_list = self.get_tag_has_music()
 
-    def recommend_music(self,user_tag,top_n=10,freq_thresh=2):
+    def recommend_music(self,user_tag,top_n=10,freq_thresh=3):
         music_id_list = self.get_music_id_list_by_user_tag(user_tag)
         music_frequency = Counter(music_id_list).most_common()
         k = min(len(music_frequency),top_n)
@@ -35,24 +35,26 @@ class FrequencyModel(RecommendModel):
 
         return recommended_music_id_list        
 
-    def recommend_user_tag(self,playlist,top_n=10,freq_thresh=2):
+    def recommend_user_tag(self,playlist,top_n=10,freq_thresh=3):
         tag_frequency = Counter(playlist).most_common()
         k = min(len(tag_frequency),top_n)
 
         user_tag_id_list=[]
+        user_tag_freq=[]
         for tag,freq in tag_frequency[:k]:
             if freq < freq_thresh:
                 break
-            user_tag_id_list.append(tag)
+            user_tag_id_list.append((tag,freq))
         
         return user_tag_id_list
 
     def get_music_id_list_by_user_tag(self,user_tag):
-        music_id_list_by_user_tag=[]
-        for tag_has_music in self.tag_has_music_list:
-            tag_id = tag_has_music['tagId']
-            music_id_list = [music_id for music_id,rank in tag_has_music['musicIdList']]
+        user_tag_id = [id for id,_ in user_tag]
+        user_tag_freq = [freq for _,freq in user_tag]
 
-            if tag_id in user_tag:
-                music_id_list_by_user_tag.extend(music_id_list)
+        music_id_list_by_user_tag=[]
+        for user_tag,user_freq in zip(user_tag_id,user_tag_freq):
+            music_tag_list = [ music_tag for music_tag,_ in self.tag_has_music_list[str(user_tag)] ]
+            music_id_list_by_user_tag.extend(music_tag_list*user_freq)
+
         return music_id_list_by_user_tag
